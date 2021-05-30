f363e675029bb99abceac3c11eeacc1c296f172c
Abhishek Kumar <abhishek.omex@gmail.com>
2021-05-31 00:19:52 +0530

    connection corrected

--- config/appConfig.js
+++ config/appConfig.js
@@ -5,7 +5,7 @@ appConfig.allowedCorsOrigin = "*";
 appConfig.env = "dev";
 appConfig.db = {
     // uri: 'mongodb+srv://admin:Abhi%40awz5@cluster0-vqxjh.mongodb.net/pharmaApp?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
-    uri: 'mongodb+srv://and_navi:Asdfgh1%40@cluster0.j3pet.mongodb.net/test?authSource=admin&replicaSet=atlas-1vgscx-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+    uri: 'mongodb+srv://and_navi:Asdfgh1%40@cluster0.j3pet.mongodb.net/ePharmacy?authSource=admin&replicaSet=atlas-1vgscx-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
 };
 appConfig.apiVersion = '/api/v1';
 model = ''
